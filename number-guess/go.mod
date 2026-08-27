f3b2e17353bf14d7985744a0586ab41487107425
Rafael Gomes <rafaelgomes1404@gmail.com>
2026-08-26 21:13:11 -0300

    feat: adding the guess number minigame

---
+++ number-guess/go.mod
@@ -0,0 +1,3 @@
+module go-study-projects/number-guess
+
+go 1.27.0
---
+++ number-guess/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"math/rand/v2"
+	"os"
+	"os/exec"
+	"runtime"
+)
+
+func clearTerminal() {
+	switch runtime.GOOS {
+
+	case "linux":
+		cmd := exec.Command("clear") // Linux example, its tested
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+
+	case "windows":
+		cmd := exec.Command("cmd", "/c", "cls") // Windows example, its tested
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+
+	case "darwin":
+		cmd := exec.Command("clear") // Linux example, its tested
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+
+	default:
+		panic("OS not supported")
+	}
+}
+
+func printInstuctions() {
+	fmt.Println(" ================== ")
+	fmt.Println(" == NUMBER GUESS == ")
+	fmt.Println(" ================== ")
+	fmt.Println(" This is a golang minigame  ")
+	fmt.Println(" Choose a number between 0 and 100 ")
+	fmt.Println(" ================== ")
+}
+
+func confirmRestart(hadSuccess bool) bool {
+	fmt.Println("")
+	if hadSuccess {
+		fmt.Println("You gessed, congrats!")
+	} else {
+		fmt.Println("You failed, Better luck next time")
+	}
+	fmt.Println("")
+
+	var userConfirmation string
+
+	fmt.Println("Type 'y' to play again.")
+	fmt.Print("> ")
+	fmt.Scan(&userConfirmation)
+
+	if userConfirmation == "y" {
+		return true
+	}
+
+	return false
+}
+
+func getUserResponse() (int, error) {
+	var response int
+	fmt.Print("> ")
+	_, error := fmt.Scan(&response)
+	if error != nil {
+		return 0, errors.New("error while processing your output, try Again")
+	}
+
+	return response, nil
+}
+
+func checkUserReponse(userResponse int) bool {
+	correctAnswer := rand.IntN(101)
+	return userResponse == correctAnswer
+}
+
+func main() {
+	shouldPlayAgain := true
+
+	for shouldPlayAgain {
+		clearTerminal()
+		printInstuctions()
+
+		response, error := getUserResponse()
+		if error != nil {
+			fmt.Println(error.Error())
+			return
+		}
+
+		hadSuccess := checkUserReponse(response)
+		shouldPlayAgain = confirmRestart(hadSuccess)
+	}
+}
