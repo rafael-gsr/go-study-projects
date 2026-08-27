211ee0966d19a49bb29d0505cfd5559e15020cef
Rafael Gomes <rafaelgomes1404@gmail.com>
2026-08-26 21:15:11 -0300

    fix: removing comments

--- number-guess/main.go
+++ number-guess/main.go
@@ -13,17 +13,17 @@ func clearTerminal() {
 	switch runtime.GOOS {
 
 	case "linux":
-		cmd := exec.Command("clear") // Linux example, its tested
+		cmd := exec.Command("clear")
 		cmd.Stdout = os.Stdout
 		cmd.Run()
 
 	case "windows":
-		cmd := exec.Command("cmd", "/c", "cls") // Windows example, its tested
+		cmd := exec.Command("cmd", "/c", "cls")
 		cmd.Stdout = os.Stdout
 		cmd.Run()
 
 	case "darwin":
-		cmd := exec.Command("clear") // Linux example, its tested
+		cmd := exec.Command("clear")
 		cmd.Stdout = os.Stdout
 		cmd.Run()
 
